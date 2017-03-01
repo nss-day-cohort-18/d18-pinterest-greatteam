4cf10a3551426fc24d1af463bbae6a2a60407feb
William A Sims III <willsims14@gmail.com>
2017-03-01 13:44:09 -0600

    Refactored file structure.

--- app/app.js
+++ app/app.js
@@ -1,1 +1,27 @@
-console.log("Running App.js");
+"use strict";
+
+var app = angular.module("PinterestApp", ["ngRoute"]);
+
+
+app.config(function($routeProvider){
+    // $routeProvider.
+    // when('/', {
+    //     templateUrl: 'login/loginUser.html',
+    //     controller: 'LoginUserCtrl'
+
+    // }).
+    // otherwise('/home');
+});
+
+
+
+// app.run(($location, FBCreds) => {
+//     let creds = FBCreds;
+//     let authConfig = {
+//         apiKey: creds.apiKey,
+//         authDomain: creds.authDomain,
+//         databaseURL: creds.databaseURL
+//     };
+
+//     firebase.initializeApp(authConfig);
+// });
---
+++ app/values/fb-creds.js
@@ -0,0 +1,7 @@
+"use strict";
+
+app.constant('FBCreds', {
+	apiKey: "",
+	authDomain: "",
+	databaseURL: ""
+});
