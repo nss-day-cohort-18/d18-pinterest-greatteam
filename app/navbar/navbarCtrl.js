63de9542d8b3aefd3933306b40ebc30b6918461c
Zak Spence <zakspence@gmail.com>
2017-03-01 15:03:39 -0600

    login and functions written into navbarCtrl.js

--- app/navbar/navbarCtrl.js
+++ app/navbar/navbarCtrl.js
@@ -1,3 +1,24 @@
-angular
-	.module('PinterstApp')
-	.controller('navbarCtrl', )
+app.controller('navbarCtrl', function($scope, FirebaseFactory, AuthFactory){
+		//initialize navbar
+		$scope.account = { email: '', password: '' };
+		$scope.isLoggedIn = false;
+
+		$scope.login = function() = {
+			console.log("you clicked login");
+	    	AuthFactory
+		    .loginUser($scope.account)
+		    .then( () => {
+		        $window.location.href = "#!/";
+		        $scope.isLoggedIn = true;
+			});
+		};
+
+		$scope.logout = function() = {
+			console.log("logout clicked");
+			AuthFactory.logoutUser()
+			.then(function(data){
+				$window.location.url = "#!/login";
+				$scope.isLoggedIn = false;
+			});
+		};
+	};
