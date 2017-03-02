8b59f6b0876cd57f328e9c1684662301df68532d
Blaise Roberts <blaiseroberts@Blaises-MacBook-Pro-2.local>
2017-03-02 13:04:49 -0600

    removed fb-creds.js

--- app/values/fb-creds.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-app.constant('FBCreds', {
-
-    apiKey: "AIzaSyD4qV-JcPLklBETJIPkFvfLl0Hj62uaqgI",
-    authDomain: "pinteret-project.firebaseapp.com",
-    databaseURL: "https://pinteret-project.firebaseio.com"
-
-});
