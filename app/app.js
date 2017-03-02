010399bda9641ddb621dd4b945b98da6e754f723
Zak Spence <zakspence@gmail.com>
2017-03-02 10:25:14 -0600

    routing is set up (in theory) and i can log in.

--- app/app.js
+++ app/app.js
@@ -8,18 +8,30 @@ app.config(function($routeProvider){
     when('/', {
         templateUrl: 'app/login/login.html'
     }).
+    when('/:userID/boards', {
+        templateUrl: 'app/myBoards/boards.html'
+    }).
+    when('/:userID/pins', {
+        templateUrl: 'app/myPins/pins.html'
+    }).
+    when('/explore', {
+    templateUrl: 'app/explore/explore.html'
+    }).
+    when('/:userID', {
+    templateUrl: 'app/profile/profile.html'
+    }).
     otherwise('/');
 });
 
 
 
-// app.run(($location, FBCreds) => {
-//     let creds = FBCreds;
-//     let authConfig = {
-//         apiKey: creds.apiKey,
-//         authDomain: creds.authDomain,
-//         databaseURL: creds.databaseURL
-//     };
+app.run(($location, FBCreds) => {
+    let creds = FBCreds;
+    let authConfig = {
+        apiKey: creds.apiKey,
+        authDomain: creds.authDomain,
+        databaseURL: creds.databaseURL
+    };
 
-//     firebase.initializeApp(authConfig);
-// });
+    firebase.initializeApp(authConfig);
+});
