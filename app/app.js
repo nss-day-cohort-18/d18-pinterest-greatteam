9283310e9d80cc727fabd4e589b28a43bc172040
William A Sims III <willsims14@gmail.com>
2017-03-02 13:30:55 -0600

    Create boards now works.

--- app/app.js
+++ app/app.js
@@ -3,13 +3,30 @@
 var app = angular.module("PinterestApp", ["ngRoute"]);
 
 
+let isAuth = (AuthFactory) => new Promise ( (resolve, reject) => {
+  console.log("running isAuth");
+    AuthFactory.isAuthenticated()
+    .then ( (userExists) => {
+    console.log("userExists", userExists);
+        if (userExists){
+      console.log("Authenticated, go ahead.");
+            resolve();
+        }else {
+      console.log("Authentication rejected, go away.");
+            reject();
+        }
+    });
+});
+
 app.config(function($routeProvider){
     $routeProvider.
     when('/', {
         templateUrl: 'app/login/login.html'
     }).
     when('/:userID/boards', {
-        templateUrl: 'app/myBoards/boards.html'
+        templateUrl: 'app/myBoards/boards.html',
+        controller: "BoardCtrl",
+        resolve: {isAuth}
     }).
     when('/:userID/pins', {
         templateUrl: 'app/myPins/pins.html'
--- app/myBoards/boardCtrl.js
+++ app/myBoards/boardCtrl.js
@@ -1,18 +1,26 @@
 "use strict";
 
-app.controller('pinCtrl', function ($scope, FirebaseFactory, AuthFactory) {
-    $scope.user = AuthFactory.getUser();
+app.controller('BoardCtrl', function ($scope, FirebaseFactory, AuthFactory) {
+    let user = AuthFactory.getUser()
+
+
+    console.log("User1: ", user);
+
     $scope.newBoard = {
     	uid: user
     };
 
+    console.log("User2: ", user);
+    console.log("NewBoard: ", $scope.newBoard);
 
     FirebaseFactory.getUserBoards(user).then(function(boardCollection){
         $scope.boards = boardCollection;
     });
 
     $scope.makeNewBoard = function(){
-		FirebaseFactory.createNewBoard($scope.newBoard).then(function(){
+        console.log("CLICKED Add New Board");
+		FirebaseFactory.createNewBoard($scope.newBoard).then(function(x){
+            console.log("X: ", x);
 			FirebaseFactory.getUserBoards(user).then(function(boardCollection){
                 $scope.boards = boardCollection;
 		    	$scope.newBoard = {
